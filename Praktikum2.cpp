234d413563c4b2e6c45378869d8ed963b40a8e1d
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-03-11 10:27:37 +0700

    Fix typo in input prompt for height

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void inputData(float &berat, float &tinggi) {
     cout << "Masukkan berat badan (kg): ";
     cin >> berat;
-    cout << "Masukkan tinggi badan (m): ";
+    cout << "Masuk tinggi badan (m): ";
     cin >> tinggi;
 }
 
